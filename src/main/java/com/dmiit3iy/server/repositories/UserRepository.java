97aeebb60c7d8cab7faeb666c9bb60b85c3fdf24
Dmiit3iy <94470190+Dmiit3iy@users.noreply.github.com>
2024-06-10 12:16:20 +0500

    Update UserRepository.java
    add annotation

--- src/main/java/com/dmiit3iy/server/repositories/UserRepository.java
+++ src/main/java/com/dmiit3iy/server/repositories/UserRepository.java
@@ -4,7 +4,7 @@ import com.dmiit3iy.server.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLogin(String login);
 }

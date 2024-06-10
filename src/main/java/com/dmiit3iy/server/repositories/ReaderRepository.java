854489ae1d160e65c219b9eddbd2f6a09eb32bf2
Dmiit3iy <94470190+Dmiit3iy@users.noreply.github.com>
2024-06-10 12:15:52 +0500

    Update ReaderRepository.java
    add annotation

--- src/main/java/com/dmiit3iy/server/repositories/ReaderRepository.java
+++ src/main/java/com/dmiit3iy/server/repositories/ReaderRepository.java
@@ -2,7 +2,7 @@ package com.dmiit3iy.server.repositories;
 
 import com.dmiit3iy.server.models.Reader;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+@Repository
 public interface ReaderRepository extends JpaRepository<Reader, Long> {
 
 }

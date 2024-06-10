842fcd54fe81a6b783709cdf674a99de008d76e5
Dmiit3iy <94470190+Dmiit3iy@users.noreply.github.com>
2024-06-10 12:14:23 +0500

    Update BookRepository.java
    add annotation

--- src/main/java/com/dmiit3iy/server/repositories/BookRepository.java
+++ src/main/java/com/dmiit3iy/server/repositories/BookRepository.java
@@ -4,7 +4,7 @@ import com.dmiit3iy.server.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByAuthorAndTitle(String author, String title);
 

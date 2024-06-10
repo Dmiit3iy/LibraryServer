312de07ece50bd87a5366fc85fee6d2e4ceaf4af
Dmiit3iy <94470190+Dmiit3iy@users.noreply.github.com>
2024-06-10 12:15:11 +0500

    Update LibrarianRepository.java
    add annotation

--- src/main/java/com/dmiit3iy/server/repositories/LibrarianRepository.java
+++ src/main/java/com/dmiit3iy/server/repositories/LibrarianRepository.java
@@ -2,6 +2,6 @@ package com.dmiit3iy.server.repositories;
 
 import com.dmiit3iy.server.models.Librarian;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+@Repository
 public interface LibrarianRepository extends JpaRepository<Librarian, Long> {
 }

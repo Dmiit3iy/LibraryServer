c11e4464d817bd4132eb312c8acf5ef1fb7d0a52
Dmiit3iy <94470190+Dmiit3iy@users.noreply.github.com>
2024-06-10 12:15:33 +0500

    Update OrderRepository.java
    add annotation

--- src/main/java/com/dmiit3iy/server/repositories/OrderRepository.java
+++ src/main/java/com/dmiit3iy/server/repositories/OrderRepository.java
@@ -2,6 +2,6 @@ package com.dmiit3iy.server.repositories;
 
 import com.dmiit3iy.server.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+@Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
 }
